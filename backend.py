0f464a139bc124d0a837f06ba8fecf304fc0fede
Tim Hunter <timh@bittorrent.com>
2013-08-29 16:14:01 -0700

    updating backend to line up with vimeo version

--- backend.py
+++ backend.py
@@ -7,12 +7,9 @@ except ImportError:
     except ImportError:
         raise ImportError("GE requires python2, 2.6 or higher, or 2.5 with simplejson.")
 import os
-import re
 import logging
-import pickle
-import md5
 
-import config
+
 
 
 class MetricsError(Exception):
@@ -22,7 +19,6 @@ class MetricsError(Exception):
 
     def __str__(self):
         return "%s (%s)" % (self.msg, self.underlying_error)
-
 
 class Backend(object):
 
@@ -49,7 +45,7 @@ class Backend(object):
             urllib2.install_opener(opener)
             # finnally get our data
             response = urllib2.urlopen(url)
-       else:
+        else:
             response = urllib2.urlopen("%s/metrics/index.json" % self.config.graphite_url)
        
         m = open('%s.tmp' % self.config.filename_metrics, 'w')
@@ -72,17 +68,6 @@ class Backend(object):
         except ValueError, e:
             raise MetricsError("Can't parse metrics file", e)
 
-    # yields metrics that match at least one of the specified patterns
-    def yield_metrics(self, match):
-        match_objects = [re.compile(regex) for regex in match]
-        metrics = self.load_metrics()
-        for metric in metrics:
-            for m_o in match_objects:
-                match = m_o.search(metric)
-                if match is not None:
-                    yield metric
-                    break
-
     def stat_metrics(self):
         try:
             return os.stat(self.config.filename_metrics)
@@ -93,25 +78,22 @@ class Backend(object):
         self.logger.debug("loading metrics")
         metrics = self.load_metrics()
 
-        self.logger.debug("updating targets")
-        targets_all = s_metrics.list_targets(metrics)
-        open(config.targets_all_cache_file, 'w').write(pickle.dumps(targets_all))
+        self.logger.debug("removing outdated targets")
+        s_metrics.remove_metrics_not_in(metrics)
 
-    def load_data(self):
-        self.logger.debug("loading targets")
-        targets_all = pickle.loads(open('targets_all.cache').read())
-        return targets_all
+        self.logger.debug("updating targets")
+        s_metrics.update_targets(metrics)
 
 
 def get_action_on_rules_match(rules, subject):
     '''
-    rules being a a list of tuples, and each tuple having 2 elements, like:
-    {'plugin': ['diskspace', 'memory'], 'what': 'bytes'},
-    action
+rules being a a list of tuples, and each tuple having 2 elements, like:
+{'plugin': ['diskspace', 'memory'], 'what': 'bytes'},
+action
 
-    the dict is a list of conditions that must match (and). if the value is an iterable, those count as OR
-    action can be whatever you want. the actions for all matching rules are yielded.
-    '''
+the dict is a list of conditions that must match (and). if the value is an iterable, those count as OR
+action can be whatever you want. the actions for all matching rules are yielded.
+'''
     for (match_rules, action) in rules:
         rule_match = True
         for (tag_k, tag_v) in match_rules.items():
